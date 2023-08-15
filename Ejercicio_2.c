f05eb15511d9c71ed983627828a28108a7981c9c
Gustavo Gatamora <39883039+gusgatam@users.noreply.github.com>
2023-08-15 11:59:22 -0300

    Update Ejercicio_2.c
    No se utiliza return en funciones que no devuelven valor.

--- Ejercicio_2.c
+++ Ejercicio_2.c
@@ -13,19 +13,19 @@ int main()
 	perimetro(a,b);
 	area(a,b);
 	system("pause");
-	return 0;
+	return 0; 
 }
 void perimetro(int a, int b)
 {
 	int p=0;
 	p=2*a+2*b;
 	printf("El valor del perimetro es: %d\n",p);
-	return;
+	//return; No se utiliza en funciones que no devuelven valor.
 }
 void area(int a, int b)
 {
 	int ar=0;
 	ar=a*b;
 	printf("El valor del area es: %d\n",a);
-	return;
+	//return; No se utiliza en funciones que no devuelven valor.
 }
