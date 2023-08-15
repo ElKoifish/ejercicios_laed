a8225525801c6015b6438a01f1d398990be2fad7
Gustavo Gatamora <39883039+gusgatam@users.noreply.github.com>
2023-08-15 12:00:14 -0300

    Update Ejercicio_4.c
    No se utiliza en funciones que no devuelven valor.

--- Ejercicio_4.c
+++ Ejercicio_4.c
@@ -4,7 +4,7 @@
 void resistencia (int R1, int R2, int R3, int R4, int R5);
 int main()
 {
-	int R1,R2,R3,R4,R5;
+	int R1,R2,R3,R4,R5; // utilizar estructuras repetitivas para la carga.
 	printf("Ingrese R1:");
 	scanf("%d",&R1);
 	printf("Ingrese R2:");
@@ -24,5 +24,5 @@ void resistencia(int R1, int R2, int R3, int R4, int R5)
 	int RT=0;
 	RT = R1+R2+R3+R4+R5;
 	printf("El valor de las resistencias en total es: %d\n",RT);
-	return;
+	//return; No se utiliza en funciones que no devuelven valor.
 }
